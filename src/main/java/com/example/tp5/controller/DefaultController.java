56a5d847f27cf804ec01736bfda4ffd6c2a84a79
Orghaniian <e163100@univ-lemans.fr>
2020-11-16 20:01:26 +0100

    suppression FragmentsController inutile ajout DefaultController

--- src/main/java/com/example/tp5/controller/DefaultController.java
+++ src/main/java/com/example/tp5/controller/DefaultController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloWorldController {
+public class DefaultController {
 
-
-    @GetMapping("/greeting")
+    @GetMapping()
     public String greeting(@RequestParam(name="nameGET", required=false, defaultValue="World") String nameGET, Model model) {
         model.addAttribute("nomTemplate", nameGET);
         return "greeting";
